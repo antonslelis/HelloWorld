97b86319b5c898d59eee3c85d803ab36fbf7baa2
Antons Lelis <3lelia74@solent.ac.uk>
2019-01-21 14:45:48 +0000

    Initial commit

---
+++ app/src/main/java/uk/ac/solent/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package uk.ac.solent.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
+        TextView tv= new TextView(this);
+        tv.setText("Hello World!");
+        setContentView(tv);*/
+    }
+}
